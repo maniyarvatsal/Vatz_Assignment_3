e50a03b368aa689538b3e71e05a598fb4631e1e1
vatsal Maniyar <maniyarvatsal5@gmail.com>
2026-01-17 15:25:03 +0530

    Delete assignment/assignment_2 directory

--- assignment/assignment_2/readme.md
+++
@@ -1,5 +0,0 @@
-# ASSIGNMENT 2
-# TASK_1
-- file : task_1.py
-# Task_2
-- file : task_2.py
--- assignment/assignment_2/task_1.py
+++
@@ -1,7 +0,0 @@
-# odd & even
-num = int(input("enter the number:"))
-print("number is ",num)
-if num % 2==0:
-    print(num ,"is even number!!")
-else:
-    print(num , "is odd number!!")
